83337ea2db084d61601862f29e67eeb2e9e04d25
Marm00taa <lucas.santos472@fatec.sp.gov.br>
2021-08-24 00:30:37 -0300

    Fvck Bolha

---
+++ LISTA 1/EXERC_C.py
@@ -0,0 +1,11 @@
+VOLUME = 0; COMPRIMENTO = 0; LARGURA = 0; ALTURA = 0
+
+print('Calculadora de volume de caixa\n')
+COMPRIMENTO=float(input('Digite o comprimento da caixa em cm: '))
+LARGURA=float(input('Digite a largura da caixa em cm: '))
+ALTURA=float(input('Digite a altura da caixa em cm: '))
+
+VOLUME = COMPRIMENTO * LARGURA * ALTURA
+VOLUME = str(VOLUME)
+
+print('A caixa tem '+VOLUME+' cm³')
