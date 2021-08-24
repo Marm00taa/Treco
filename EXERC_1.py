12031dc951e462493a752adb06522b4c194f5726
Marm00taa <lucas.santos472@fatec.sp.gov.br>
2021-08-24 00:08:04 -0300

    teste

--- EXERC_1.py
+++ EXERC_1.py
@@ -2,9 +2,9 @@ f = 0
 c = 0
 
 print('Conversor de °C para °F \n')
-c = float(input('digite a temperatura em °C: '))
+c = float(input('digite a temperatura em °C: \n'))
 
 f = (9 * c + 160) / 5
 f=str(f)
 
-print(f+' °F')
+print(f+' °F teste')
