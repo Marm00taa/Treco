0ad46fa77cd7680bfd5f9db55a1841d7da20c4ab
Marm00taa <lucas.santos472@fatec.sp.gov.br>
2021-08-31 23:45:08 -0300

    EU ODEIO A FACULDADE

---
+++ LISTA 5/E501.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import time
+
+NumLados = 0
+MedLado1 = 0
+MedLado2 = 0
+MedLado3 = 0
+A = 0
+P = 0
+
+NumLados = float (input ('Digite o Numero de Lados: '))
+
+if NumLados < 3:
+    print ('Não é um poligono')
+    sys.exit()
+
+
+elif NumLados == 3:
+    print ('TRIÂNGULO')
+    MedLado1  = float (input ('Digite o primeiro lado: '))
+    MedLado2  = float (input ('Digite o segundo lado: '))
+    MedLado3  = float (input ('Digite o terceiro lado: '))
+    P= (MedLado1 + MedLado2 + MedLado3) / 2
+    A= (P*(P-MedLado1)*(P-MedLado2)*(P-MedLado3)**(1/2))
+    print(f'\n A area do triangulo é: {A: 5.2f}''cm² (aproximadamente) ')
+    
+    
+
+elif NumLados == 4:
+    print ('QUADRADO')
+    MedLado1 = float (input ('Digite o lado do quadrado: '))
+    A= MedLado1 * MedLado1
+    print(f'\n A area do quadrado é de: {A: 5.2f}''cm²')
+
+elif NumLados == 5:
+    print ('PENTAGONO')
