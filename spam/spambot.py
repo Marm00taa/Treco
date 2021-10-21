fef6762e1a69d54218ed6e7bacafeeea2d9da7ad
Marm00taa <lucas.santos472@fatec.sp.gov.br>
2021-10-21 10:29:43 -0300

    baderna do krl já

--- spam/spambot.py
+++ spam/spambot.py
@@ -1,11 +1,7 @@
 import pyautogui, time
 
 time.sleep(5)
-text = open('F:\PROGRAMAÇÃO\CÓDIGOS RUINS DA FACULDADE\BadernaDaFaculdade\spam\script.csv')
+text = open('spam/script.csv')
 for each_line in text:
     pyautogui.typewrite(each_line)
     pyautogui.press("enter")
-
-#with open("F:\PROGRAMAÇÃO\CÓDIGOS RUINS DA FACULDADE\BadernaDaFaculdade\spam\script.csv") as f:
-#    for lines in f:
-#        print(lines)
